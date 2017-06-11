6c33c25673c5a7aa9a81786e8a00c567cf35e005
puppetmaster3 <vicmasons@gmail.com>
2017-06-11 09:43:17 -0400

    added second comp

--- demo-srv/root/_js/main.js
+++ demo-srv/root/_js/main.js
@@ -5,10 +5,10 @@ loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
 	success: function(){
 		console.log('almost ready?')
 		loadjs([
-			'/_js/vendor/jquery.jsForm.min.js'
+			'https://cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js'
 			,'https://cdn.rawgit.com/topseed/topseed-turbo/master/vendor/raphael.min.js'
 
-			,'/_js/vendor/circle-progress.min.js'
+			,'/_js/vendor/circlesMOD.js'
 
 			], { success: function(){
 				console.log('almost ready!')
