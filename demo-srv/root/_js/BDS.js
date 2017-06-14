92e70992dd99f5cc5d40d337e7a88d85cd06f495
puppetmaster3 <vicmasons@gmail.com>
2017-06-14 17:18:00 -0400

    released 3.3

--- demo-srv/root/_js/BDS.js
+++ demo-srv/root/_js/BDS.js
@@ -1,18 +1,15 @@
 'use strict'
 console.log('BDS')
+var BDS = Class.extend({ //IE11-compatible base class for Data Access Object
 
-let ROOT = 'http://localhost:8081/'
-
-class BDS {
-
-	static fetch(fetch_,ROOT_, url_, data_) {
+	 _fetch: function(fetch_,ROOT_, url_, data_) { //static
+		//var xjt_ = Cookies.get(BDS.XJT)
+		//var xb_  = Cookies.get(BDS.XBASIC)
 		console.log('fetching ', url_)
 		return fetch_(ROOT_ + url_ , { //1 call
 				method: 'post'
 				, headers: {
-					'Content-Type': 'application/json',
-					'Accept':'application/json',
-					 credentials: 'same-origin' //res.cookie returned
+					'Content-Type': 'application/json'
 				}
 				, body: JSON.stringify(data_)
 			}).then(function(response) { //2 returns a promise
@@ -27,7 +24,7 @@ class BDS {
 			})
 	}//_()
 
-} // class
+}) //'class'
 
 // for node:
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
