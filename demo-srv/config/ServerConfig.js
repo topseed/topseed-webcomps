f54f2701e3aa4c61f4e060f951e1338dc15b96cc
Wolfgang <wgehner@gmail.com>
2017-06-14 12:52:47 -0700

    Change port and package.json

--- demo-srv/config/ServerConfig.js
+++ demo-srv/config/ServerConfig.js
@@ -2,7 +2,7 @@
 
 class ServerConfig {
 	get WEBROOT() {return 'ROOT'}
-	get SSR_PORT() {return 8080}
-	get WWW_PORT()  {return 8081}
+	get SSR_PORT() {return 9080}
+	get WWW_PORT()  {return 9081}
 	
 } module.exports = ServerConfig
---
+++ srv/config/ServerConfig.js
@@ -0,0 +1,7 @@
+'use strict'
+
+class ServerConfig {
+	get WEBROOT() {return 'webroot'}
+	get WWW_PORT() {return 8000}
+	get WEB_SUBDOMAIN() {return 'www'}
+} module.exports = ServerConfig
