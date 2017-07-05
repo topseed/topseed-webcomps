01005072359e70c72d84d6c69fe8ba94ee300ca8
Wolfgang <wgehner@gmail.com>
2017-07-05 07:27:38 -0700

    Move doT to main.js

--- demo-srv/root2/_js/main.js
+++ demo-srv/root2/_js/main.js
@@ -7,7 +7,7 @@ function loadLibs(){
 	//most of these could be in manifest
 	return Promise.all([
 		TS.load('https://cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
-		, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js') 
+		, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js')
 		, TS.load('https://cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init}) //Support for Standard Web Component
 		, TS.load('//cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js') //Support for RIOT
 		, TS.load('https://rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-latest.js')
