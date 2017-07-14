ad60be980a25ee2ae26868767b461c47a3543da9
Wolfgang <wgehner@gmail.com>
2017-07-14 15:14:17 -0700

    ES5 chart

--- dev-srv/index.js
+++ dev-srv/index.js
@@ -26,6 +26,6 @@ var nightmare = Nightmare()
 server.use(express.static(ServerConfig.WEBROOT))
 
 server.listen(ServerConfig.WWW_PORT, '0.0.0.0', function() {
-	console.log('App listening on port '+ServerConfig.WWW_PORT)
+	console.log('App listening on http://localhost:'+ServerConfig.WWW_PORT)
 	console.log('Press Ctrl+C to quit.')
 })
--- dev-srv/root/_js/main.js
+++ dev-srv/root/_js/main.js
@@ -1,28 +1,37 @@
-'use strict'
-loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
-	success: function(){
+var TM = {
 
-		libsLoaded()
+	loadLibs: function(){
+		console.log('loadLibs called')
 
-	}//suc
-})//load
+		//most of these could be in cache.mf
+		return Promise.all([
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
+			, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js')
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init()}) //Support for Standard Web Components
+			//, TS.load('//cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js') //Support for RIOT
+			, TS.load('//rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-latest.js')
+			//, TS.load('/_js/BLX.js')
+			//, TS.load('/_js/BDS.js')
+		])
+		.then(TM.libsLoaded)
+	}
 
-function libsLoaded(){
-	console.log('Loaded libs')
-	
-	TS.signalAppReady()
+	, libsLoaded: function(){
+		
+		TS.signalAppReady()
 
-	TT.ScontentID ='#content-wrapper'
-	TT.handle(function(evt) {
-		console.log(':')
-		if(TT.PRE==evt.typ)  {//start
-			console.log(evt.$new)
-			//$('#content-wrapper').fadeTo(100,.2)
-		}
-		if(TT.PAGE==evt.typ)  {//new pg loaded
-			$(TT.ScontentID).html(evt.$new)
-			//$('#content-wrapper').fadeTo(100,1)
+		TT.ScontentID = '#content-wrapper'
+		TT.handle(function(evt) {
+			if(TT.PRE == evt.typ)  {
+				//$('#content-wrapper').fadeTo(100,.2)
+			}
+			if(TT.PAGE == evt.typ)  {
+				$(TT.ScontentID).html(evt.$new)
+			}
+		})
+	}
 
-		}
-	})
-}
+} //class
+
+TM.loadLibs()
+
