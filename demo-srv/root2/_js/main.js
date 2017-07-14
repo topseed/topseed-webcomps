ef40a5bec78057c123c297b4eff6f31ee0d13ea5
Wolfgang <wgehner@gmail.com>
2017-07-14 08:36:36 -0700

    Use setup-6.0

--- demo-srv/root2/_js/main.js
+++ demo-srv/root2/_js/main.js
@@ -1,28 +1,43 @@
-loadjs.ready(['polyfills', 'keyLibs'], {// loaded setup libs
-	success: function(){
-	loadjs([
-		'/_js/BLX.js'
-		, '/_js/BDS.js'
+//requires setup-6.0.js or higher
+var TM = {
 
-		], { success: function(){
-			libsLoaded()
-		}
-	})//loadjs
-	}//suc
-})
+	loadLibs: function(){
+		console.log('loadLibs called')
 
-//========================================================
-function libsLoaded(){
-	
-	TS.signalAppReady()
+		//most of these could be in cache.mf
+		return Promise.all([
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
+			, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js')
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init()}) //Support for Standard Web Components
+			, TS.load('//cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js') //Support for RIOT
+			, TS.load('//rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-latest.js')
+			, TS.load('/_js/BLX.js')
+			, TS.load('/_js/BDS.js')
+		])
+		.then(TM.libsLoaded)
+	}
 
-	TT.ScontentID = '#content-wrapper'
-	TT.handle(function(evt) {
-		if(TT.PRE == evt.typ)  {
-			//$('#content-wrapper').fadeTo(100,.2)
-		}
-		if(TT.PAGE == evt.typ)  {
-			$(TT.ScontentID).html(evt.$new)
-		}
-	})
-}
+	, libsLoaded: function(){
+		
+		TS.signalAppReady()
+
+		TT.ScontentID = '#content-wrapper'
+		TT.handle(function(evt) {
+			if(TT.PRE == evt.typ)  {
+				//$('#content-wrapper').fadeTo(100,.2)
+			}
+			if(TT.PAGE == evt.typ)  {
+				$(TT.ScontentID).html(evt.$new)
+			}
+		})
+	}
+
+} //class
+
+TM.loadLibs()
+
+
+
+
+
+
---
+++ demo-srv/root2/_js/setup-6.0.js
@@ -0,0 +1,128 @@
+// in 'main.js': TS.signalAppReady() // app ready sent 
+/*ex pg use:
+	TS.onAppReady(UIinit)
+
+	function UIinit() {
+	}
+*/
+//'use strict'  // NOT in IE 11 w/ Class we can't
+
+//polyfill for _load endsWith
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(searchString, position) {
+		var subjectString = this.toString()
+		if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+			position = subjectString.length
+		}
+		position -= searchString.length
+		var lastIndex = subjectString.lastIndexOf(searchString, position)
+		return lastIndex !== -1 && lastIndex === position
+	}
+}
+
+var TS = { //class:
+
+	_load: function(url, resolve, reject){
+		var isCss = url.toLowerCase().endsWith('.css')
+		var el = document.createElement(isCss?'css':'script')
+		el.onload = function(){ //IE9 min
+			resolve(url)
+		}
+		el.onerror = function(){ 
+			reject(url)
+		}
+		if (isCss)
+		{
+			el.href = url
+			el.type = 'text/css'
+			el.rel = 'stylesheet'
+			var ref = window.document['head'].appendChild(el)
+		}
+		else {
+			console.log('_load:'+url)
+			el.src = url
+			el.async = true
+			var ref = window.document.getElementsByTagName('script')[0]
+			ref.parentNode.insertBefore(el, ref)
+		}
+	}
+
+	, load: function(url){
+		return new Promise(function (resolve, reject) {
+			TS._load(url, resolve, reject)
+		})
+	}
+
+	, appReady: false
+
+	, signalAppReady: function() {
+		TS.appReady = true
+	}
+
+	, onAppReady: function(pinit) {
+		if (TS.appReady  && 'undefined' != typeof jQuery) { // wait for libs loaded.
+			console.log('app-ready!')
+			pinit()
+		} else {
+			setTimeout(function() {//wait X milliseconds then loop and recheck if ready
+				console.log(',') // likey TS.signalAppReady() was not called
+				TS.onAppReady(pinit)//loop
+			} ,60)
+		}//else
+	}//()
+
+	, loadOnAppReady: function(lib, pinit){
+		if(TS.appReady) {
+			console.log('main?')
+			TS.load(lib).then(pinit)
+		} else {
+			setTimeout(function() {//wait X milliseconds then loop and recheck if ready
+				console.log(',') // likey TS.signalAppReady() was not called
+				TS.loadOnAppReady(lib, pinit)//loop
+			} ,60)
+		}//else
+	}
+
+ 	, loadIE: function() { 
+		if (bowser.msie || !bowser.a)
+		{
+			return Promise.all([
+				TS.load('//cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js')
+				, TS.load('//cdn.jsdelivr.net/picturefill/3.0.3/picturefill.min.js')
+			])
+		}	
+		return Promise.resolve('isNotIE')
+	}
+
+	, loadKeylibs: function() {
+		return Promise.all([
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/js.cookie.min.js')
+			, TS.load('//unpkg.com/topseed-util@24.0.0/PLX.js') // key part for comp com
+		])
+	}
+
+	, loadFRPAndBowser: function() {
+		return Promise.all([
+			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/flyd.min.js')
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/bowser.min.js').then(TS.loadIE)
+			, TS.loadKeylibs()
+		])
+		.then(function(){TS.load('/_js/main.js')});
+	}
+
+	, loadPromise: function() {
+		if (!window.Promise)
+			TS._load('//cdn.jsdelivr.net/es6-promise-polyfill/1.2.0/promise.min.js', TS.loadFRPAndBowser)
+		else
+			TS.loadFRPAndBowser() 
+	}
+
+}//class
+
+// load stuff:
+TS.loadPromise() //etc
+
+
+window.onbeforeunload = function (e) {
+	console.log('please come back soon')
+}
