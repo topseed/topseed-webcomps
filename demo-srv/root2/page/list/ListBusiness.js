7130d65e688b1b8e8060cda628fe05f19187dd44
Wolfgang <wgehner@gmail.com>
2017-07-14 10:51:31 -0700

    Cleanup

--- demo-srv/root2/page/list/ListBusiness.js
+++ demo-srv/root2/page/list/ListBusiness.js
@@ -6,38 +6,38 @@ function ListBusiness() {// 'closure|module'-iso.
 	
 	var SimpleBusiness = BLX.extend({
 
-        list: function(listId, templateId) {
+		list: function(listId, templateId) {
 			sb.siteListDao.selectList().then(function(values) {
-                    //http://www.javascriptoo.com/dot-js
-                    var templateText = document.getElementById(templateId).text
-                    var templateFunction = doT.template(templateText)
-                    document.getElementById(listId).innerHTML = templateFunction({'array': values})
+					//http://www.javascriptoo.com/dot-js
+					var templateText = document.getElementById(templateId).text
+					var templateFunction = doT.template(templateText)
+					document.getElementById(listId).innerHTML = templateFunction({'array': values})
 
-                }).catch(function(error) {
+				}).catch(function(error) {
 			  		console.log('ListBusiness.selectList error: '+error.message);
 				}
-            );  
+			);  
 		}
 
-        , compList: function(componentName) {
-            //if (1==1) return
+		, compList: function(componentName) {
+			//if (1==1) return
 			sb.siteListDao.selectList().then(function(values) {
 
-                    console.log('got data')
-                    var comp = document.querySelector(componentName) //good if only one
-                    console.log('got component'+comp)
-                    comp.list(values)
+					console.log('got data')
+					var comp = document.querySelector(componentName) //good if only one
+					console.log('got component'+comp)
+					comp.list(values)
 
-                }).catch(function(error) {
+				}).catch(function(error) {
 			  		console.log('ListBusiness.selectList error: '+error.message);
 				}
-            );  
+			);  
 		}
 
-        , addComp: function(componentSelector) {
-            var comp = document.querySelector(componentSelector)
-            comp.init(sb) //pass in sb as message bus
-        }
+		, addComp: function(componentSelector) {
+			var comp = document.querySelector(componentSelector)
+			comp.init(sb) //pass in sb as message bus
+		}
 
 	})//'class'
 
